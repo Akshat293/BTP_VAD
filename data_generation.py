58281039f3fb4a4e478261b10b515795556bc7f9
Akshat Saxena <76419575+tom635@users.noreply.github.com>
2023-02-23 13:40:14 +0530

    Adding the data processing algorithm

---
+++ data_generation.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import wave
+import pylab
+from pathlib import Path
+from scipy import signal
+from scipy.io import wavfile
+from sklearn.metrics import confusion_matrix
+import itertools
+
+INPUT_DIR = '/Users/akshatsaxena/Desktop/VAD/recordings/'
+OUTPUT_DIR = '/Users/akshatsaxena/Desktop/VAD/spectrograms/'
+
+
+parent_list = os.listdir(INPUT_DIR)
+
+
+
+
+# Utility function to get sound and frame rate info
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.frombuffer(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+
+# For every recording, make a spectogram and save it as label_speaker_no.png
+if not os.path.exists(os.path.join(OUTPUT_DIR, 'audio-images')):
+    os.mkdir(os.path.join(OUTPUT_DIR, 'audio-images'))
+    
+for filename in os.listdir(INPUT_DIR):
+    if "wav" in filename:
+        file_path = os.path.join(INPUT_DIR, filename)
+        file_stem = Path(file_path).stem # file_stem is the name of the file without the extension
+        target_dir = f'class_{file_stem[0]}'
+        dist_dir = os.path.join(os.path.join(OUTPUT_DIR, 'audio-images'), target_dir)
+        file_dist_path = os.path.join(dist_dir, file_stem)
+        if not os.path.exists(file_dist_path + '.png'):
+            if not os.path.exists(dist_dir):
+                os.mkdir(dist_dir)
+            file_stem = Path(file_path).stem
+            sound_info, frame_rate = get_wav_info(file_path)
+            print(sound_info, frame_rate)
+            pylab.specgram(sound_info, Fs=frame_rate)
+            pylab.savefig(f'{file_dist_path}.png')
+            pylab.close()
+
+# Print the ten classes in our dataset
+path_list = os.listdir(os.path.join(OUTPUT_DIR, 'audio-images'))
+print("Classes: \n")
+for i in range(10):
+    print(path_list[i])
+    
+# File names for class 1
+path_list = os.listdir(os.path.join(OUTPUT_DIR, 'audio-images/class_1'))
+print("\nA few example files: \n")
+for i in range(10):
+    print(path_list[i])
+
+
